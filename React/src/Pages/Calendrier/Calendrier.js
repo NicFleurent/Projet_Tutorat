1f616ed132091206485f556da7dcb5c0d7d22339
MirolieTheroux <mirolie010@gmail.com>
2024-04-09 09:36:33 -0400

    debut calendrier

---
+++ React/src/Pages/Calendrier/Calendrier.js
@@ -0,0 +1,19 @@
+import { StyleSheet, View} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Calendrier() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View >
+              
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+    }
+  });
