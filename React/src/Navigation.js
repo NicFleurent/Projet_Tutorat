a03ed3670a77f568c5bcb56e7d54d911a4f7b10c
MirolieTheroux <mirolie010@gmail.com>
2024-04-13 16:50:13 -0400

    ajout onglet en bas

--- React/src/Navigation.js
+++ React/src/Navigation.js
@@ -1,28 +1,52 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SplashScreen from "./Pages/SplashScreen";
 import Onboarding01 from "./Pages/Onboarding/Onboarding01";
+import PageDemande from "./Pages/DemandesAidesTuteurs/PageDemande";
+
 
 const Stack = createNativeStackNavigator();
+const StackTabs = createBottomTabNavigator();
 
-const MyStack = () => {
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Onboarding01"
+        component={Onboarding01}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PageDemande"
+        component={TabNavigator}
+        options={{ headerShown: true, title: "Retour" }}
+      />
+ 
+    </Stack.Navigator>
+  );
+}
+
+function TabNavigator() {
+  return (
+    <StackTabs.Navigator>
+      {/* Mettre les pages qui seront dans les onglets en bas */}
+      <StackTabs.Screen name="page" component={PageDemande} options={{ headerShown: false }} />
+      
+    </StackTabs.Navigator>
+  );
+}
+
+export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Onboarding01"
-          component={Onboarding01}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <StackNavigator />
     </NavigationContainer>
   );
-};
-
-export default MyStack;
+}
