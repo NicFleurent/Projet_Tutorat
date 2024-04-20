b4ddfbf7eaa35085171a5d4cba08a8e42ece099f
MirolieTheroux <mirolie010@gmail.com>
2024-04-20 16:37:13 -0400

    ajout code disponibilités

--- React/src/Pages/DemandesAidesTuteurs/Disponibilites.js
+++ React/src/Pages/DemandesAidesTuteurs/Disponibilites.js
@@ -1,20 +1,36 @@
-import { StyleSheet, View} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, View, Text, Image } from "react-native";
+import ParametreOption from "../../Components/ParametreOption";
 
-export default function Calendrier() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View >
-              
-            </View>
-        </SafeAreaView>
-    );
+export default function Disponibilites() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titre}>Disponibilités</Text>
+      <Text style={styles.sousTitre}>
+        Veuillez choisir vos disponibilités
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        
-      flex: 1,
-      backgroundColor: "#fff",
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 10,
+  },
+  titre: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 100,
+  },
+  sousTitre: {
+    fontSize: 12,
+    marginTop: 10,
+  },
+  parameterView: {
+    marginTop: 45,
+  },
+  parameterViewTest: {
+    marginTop: 30,
+  },
+});
