19131b68686b383eb884bd36b031f9c1a84fc7b9
MirolieTheroux <mirolie010@gmail.com>
2024-04-26 11:22:55 -0400

    affichage des dispo selon la journée

--- React/src/Pages/DemandesAidesTuteurs/Calendrier.js
+++ React/src/Pages/DemandesAidesTuteurs/Calendrier.js
@@ -1,40 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, TouchableOpacity, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from '../../Components/CustomButton';
+import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
-import axios from "axios"; // Importez axios
 
 const jourSemaine = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'];
 
-const Item = ({ item }) => (
-    <View style={styles.item}>
-        <Text>{item.tuteur.nom} {item.tuteur.prenom}</Text>
-        <Text>{item.heure}</Text>
-    </View>
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+
+    <TouchableOpacity onPress={onPress} style={[styles.item, { backgroundColor }]}>
+        <View style={styles.textFlatlist}>
+            <Text style={{ color: textColor }}>{'Nom : ' + item.tuteur.nom} {item.tuteur.prenom}</Text>
+            <Text style={{ color: textColor }}>{'Heure : ' + item.attributes.heure}</Text>
+        </View>
+    </TouchableOpacity>
+
 );
 
 export default function Calendrier({ route }) {
+    const navigation = useNavigation();
     const { idCours } = route.params;
     const [selectedJour, setSelectedJour] = useState();
     const [disponibilites, setDisponibilites] = useState([]);
+    const [selectedId, setSelectedId] = useState();
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`${process.env.EXPO_PUBLIC_API_URL}disponibilites/${idCours}`);
-                setDisponibilites(response.data);
-            } catch (error) {
-                console.log('Erreur lors de la récupération des disponibilités:', error);
-            }
-        };
-        if (selectedJour) {
-            fetchData();
-        }
-    }, [selectedJour]);
-console.log(disponibilites)
+        axios.get(`${process.env.EXPO_PUBLIC_API_URL}disponibilites/${idCours}`)
+            .then((response) => setDisponibilites(response.data))
+            .catch((error) => console.log(error))
+    }, []);
+
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.id === selectedId ? '#092D74' : '#E8ECF2';
+        const color = item.id === selectedId ? 'white' : 'black';
+
+        return (
+            <Item
+                item={item}
+                onPress={() => setSelectedId(item.id)}
+                backgroundColor={backgroundColor}
+                textColor={color}
+            />
+        );
+    };
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.titrePage}>Journée</Text>
+            <Text style={styles.titrePage}>Choisir la journée</Text>
             <View style={styles.buttonLayout}>
                 {jourSemaine.map((jour, index) => (
                     <TouchableOpacity
@@ -53,16 +66,22 @@ console.log(disponibilites)
                                 },
                             ]}
                         >
-                            {jour.slice(0,3)}
+                            {jour.slice(0, 3)}
                         </Text>
                     </TouchableOpacity>
                 ))}
             </View>
+            <Text style={styles.titre}>Choisir un tuteur et heure</Text>
             <FlatList
-                data={disponibilites}
-                renderItem={({ item }) => <Item item={item} />}
+                data={disponibilites.filter(dispo => dispo.attributes.journee === selectedJour)}
+                renderItem={renderItem}
                 keyExtractor={item => item.id.toString()}
+                extraData={selectedId}
             />
+
+            <CustomButton text={'Envoyer la demande de tutorat'} onPress={() => {
+                navigation.navigate("Accueil");
+            }} />
         </SafeAreaView>
     );
 }
@@ -74,9 +93,15 @@ const styles = StyleSheet.create({
         padding: 15
     },
     titrePage: {
-        fontSize: 24,
+        fontSize: 22,
         fontWeight: 'bold',
         textAlign: 'left',
+    },    
+    titre: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        marginTop:10
     },
     buttonLayout: {
         marginTop: 20,
@@ -98,10 +123,13 @@ const styles = StyleSheet.create({
     item: {
         backgroundColor: '#fff',
         padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
+        marginTop: 10,
         borderRadius: 7,
         borderWidth: 1,
         borderColor: '#ccc'
+    },
+    textFlatlist: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     }
 });
