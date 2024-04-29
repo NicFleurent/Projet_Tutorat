a89a5ab89a54d5232000f2ebba9b1d617a67abea
NicFleurent <fleurent.nicolas@hotmail.com>
2024-04-29 13:50:44 -0400

    finalisation controler et resource

--- Laravel/app/Http/Controllers/RencontresController.php
+++ Laravel/app/Http/Controllers/RencontresController.php
@@ -8,7 +8,10 @@ use App\Models\Rencontre;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
 use App\Http\Resources\RecontresResource;
+use App\Models\Jumelage;
 use App\Traits\RencontreTrait;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class RencontresController extends Controller
 {
@@ -33,6 +36,18 @@ class RencontresController extends Controller
 
     public function prochainesRencontres()
     {
+        $user_id = Auth::user()->id;
+
+        $jumelages = Jumelage::where('tuteur_id', $user_id)
+                                ->orWhere('aider_id', $user_id)
+                                ->get();
+
+        $idJumelages = [];
+
+        foreach($jumelages as $jumelage){
+            array_push($idJumelages, $jumelage->id);
+        }
+
         /*$tuteurs = TuteurCours::where('cours_id',$idCours)
                               ->where('demande_accepte',1)->get();
 
@@ -50,7 +65,14 @@ class RencontresController extends Controller
             $dispo->heure = Carbon::parse($dispo->heure)->format('H:i');
         }*/
         
-        $rencontres = Rencontre::all();
+        $rencontres = Rencontre::whereIn('jumelage_id', $idJumelages)
+                                ->whereDate('date', '>', Carbon::now())
+                                ->orderBy('date')
+                                ->limit(3)
+                                ->get();
+        foreach ($rencontres as $rencontre) {
+            $rencontre->heure = Carbon::parse($rencontre->heure)->format('H:i');
+        }
 
         return response()->json(RecontresResource::collection($rencontres), 200);
     }
