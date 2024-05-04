6b17348986984158ffc3232a94f6f7cca9f9753e
TheWiseToad <alexandergreer@hotmail.fr>
2024-05-04 17:57:02 -0400

    Fini css Login

--- Laravel/resources/views/login.blade.php
+++ Laravel/resources/views/login.blade.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
+
+    <div class="left-rectangle"></div>
+
+    <div class="header">
+        <h1>Tutorat</h1>
+        <h2>Panneau d'administration</h2>
+    </div>
+
+    <div class="right-rectangle"></div>
+
     @if(session('erreur'))
         <div class="alert alert-danger">
             {{ session('erreur') }}
@@ -18,10 +29,10 @@
         <div class="main-container">
             <label for="email">Email:</label>
             <input type="text" class="form-control" id="email" name="email" required>
-            
+
             <label for="password">Mot de passe:</label>
             <input type="password" class="form-control" id="password" name="password" required>
-</br>
+
             <button type="submit" class="button">Se connecter</button>
         </div>
         
