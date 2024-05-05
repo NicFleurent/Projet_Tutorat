1569b9f02c02066f06c42d60784aeb053e79bbca
NicFleurent <fleurent.nicolas@hotmail.com>
2024-05-05 19:13:51 -0400

    get notification

--- Laravel/app/Http/Controllers/NotificationsController.php
+++ Laravel/app/Http/Controllers/NotificationsController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\NotificationsResource;
+use App\Models\Notification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationsController extends Controller
 {
-    //
+    public function index()
+    {
+        $user_id = Auth::user()->id;
+
+        $notification = Notification::where('user_id', $user_id)->get();
+
+        return response()->json(NotificationsResource::collection($notification), 200);
+    }
 }
